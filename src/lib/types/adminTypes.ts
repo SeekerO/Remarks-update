d648fc34589bf565c275a6da1c7b2787534cb46f
SeekerDev <119729803+SeekerO@users.noreply.github.com>
2026-05-02 09:10:25 +0800

    Update adminTypes.ts

--- src/lib/types/adminTypes.ts
+++ src/lib/types/adminTypes.ts
@@ -230,7 +230,7 @@ export const navItems: NavItem[] = [
         href: "/dtrextractor",
         icon: FileCog,
         active: true,
-        pagePermissionId: "dtrextractor",
+        pagePermissionId: "dtrexporter",
         sublinks: [],
       },
     ],
